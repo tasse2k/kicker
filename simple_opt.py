0c241d50c5c17d6a444f4b777bd8f2314710fa0e
tasse2k <92542768+tasse2k@users.noreply.github.com>
2023-08-12 18:22:54 +0200

    Update simple_opt.py

--- simple_opt.py
+++ simple_opt.py
@@ -43,35 +43,52 @@ def print_team(team, iteration):
 # Optimization logic
 best_indices = initial_indices.copy()
 best_score, best_cost = calculate_score_and_cost(best_indices)
+reset_counter = 0
 
 # Optimization logic after initial optimization
-for iteration in range(1, 11):
-    # Print the current team
-    team = players_data.loc[best_indices]
-    print_team(team, iteration)
+while reset_counter < 10:
+    current_indices = best_indices.copy()
+    improved = False
     
-    # Select a random player to replace
-    random_idx = random.choice(best_indices)
+    # Iterate over each player in the team to find a replacement
+    for idx in current_indices:
+        # Iterate over each player in the dataset to find a replacement
+        for replacement_idx in players_data.index:
+            # Skip if the replacement player is already in the team
+            if replacement_idx in current_indices:
+                continue
+            
+            # Check if the replacement player has the same position
+            if players_data.loc[idx, 'position'] != players_data.loc[replacement_idx, 'position']:
+                continue
+            
+            # Replace the player and check the new score and cost
+            new_indices = [replacement_idx if i == idx else i for i in current_indices]
+            new_score, new_cost = calculate_score_and_cost(new_indices)
+            
+            # If the new team is valid and the score is improved, update the current team
+            if new_score > best_score and new_cost <= BUDGET_CONSTRAINT and is_valid_team(new_indices):
+                best_score = new_score
+                best_indices = new_indices
+                improved = True
+                break
     
-    # Iterate over each player in the dataset to find a replacement
-    for replacement_idx in players_data.index:
-        # Skip if the replacement player is already in the team
-        if replacement_idx in best_indices:
-            continue
-        
-        # Check if the replacement player has the same position
-        if players_data.loc[random_idx, 'position'] != players_data.loc[replacement_idx, 'position']:
-            continue
-        
-        # Replace the player and check the new score and cost
-        new_indices = [replacement_idx if i == random_idx else i for i in best_indices]
-        new_score, new_cost = calculate_score_and_cost(new_indices)
-        
-        # If the new team is valid and the score is improved or the same, update the best team
-        if new_score >= best_score and new_cost <= BUDGET_CONSTRAINT and is_valid_team(new_indices):
-            best_score = new_score
-            best_indices = new_indices
-            break
+    if improved:
+        print_team(players_data.loc[best_indices], 'Current Best')
+        continue
+
+    # Random replacement logic
+    random_idx = random.choice(current_indices)
+    random_replacement_idx = random.choice(players_data[players_data['position'] == players_data.loc[random_idx, 'position']].index)
+    current_indices = [random_replacement_idx if i == random_idx else i for i in current_indices]
+    new_score, new_cost = calculate_score_and_cost(current_indices)
+
+    if new_score > best_score and new_cost <= BUDGET_CONSTRAINT and is_valid_team(current_indices):
+        best_score = new_score
+        best_indices = current_indices
+        print_team(players_data.loc[best_indices], 'Current Best')
+    else:
+        reset_counter += 1
 
 # Display the final results
 best_players = players_data.loc[best_indices]
